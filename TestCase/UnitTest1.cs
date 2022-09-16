9e9eb3da1b17dcfaf35906cbb612405c63e9537d
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2022-09-16 12:18:54 +0700

    Quiz01

--- TestCase/UnitTest1.cs
+++ TestCase/UnitTest1.cs
@@ -5,7 +5,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        int number = 2;
-        Assert.Equal(number, 2);
+        string text = InEx.Revenue();
+        Assert.Equal(text, "Total revenue : 236,154 Baht");
+    }
+
+    public void Test2()
+    {
+        string t = InEx.Expenses();
+        Assert.Equal(t, "Total expenses : 213,069.55 Baht");
     }
 }
